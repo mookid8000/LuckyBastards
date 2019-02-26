d3c8b8d79e53c60d2dbccbd1e84e67423856caa9
Mogens Heller Grabe <mookid8000@gmail.com>
2019-02-26 22:33:06 +0100

    boom!

---
+++ LuckyBastards/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Tababular;
+
+namespace LuckyBastards
+{
+    class Program
+    {
+        const string FilePath = @"C:\temp\<path to file>";
+
+        static readonly TableFormatter Formatter = new TableFormatter(new Hints { CollapseVerticallyWhenSingleLine = true });
+
+        static async Task Main()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            var columns = lines.First().Split('\t');
+
+            var rows = lines.Skip(1)
+                .Select(line => line.Split('\t'))
+                .Where(cells => cells.Length == columns.Length)
+                .ToList();
+
+            PrintRows(rows, columns);
+
+            var rowsInRandomOrder = await rows.InRandomOrder();
+
+            using (var enumerator = rowsInRandomOrder.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine("Tryk (N) for NÆSTE VINDER, (Q) for at slutte.");
+
+                    if (Console.ReadKey(true).KeyChar == 'q') break;
+
+                    Console.WriteLine("VINDEREN ER:");
+                    PrintRows(new[] { enumerator.Current }, columns);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Slut");
+            Console.ReadLine();
+        }
+
+        static void PrintRows(IEnumerable<string[]> rows, string[] columns)
+        {
+            var dictionaries = rows
+                .Select(cells => cells.Zip(columns, (cell, header) => (cell: cell, header: header)))
+                .Select(tuples => tuples.ToDictionary(a => a.header, a => a.cell))
+                .ToList();
+
+            Console.WriteLine(Formatter.FormatDictionaries(dictionaries));
+        }
+    }
+}
---
+++ LuckyBastards/RandomExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LuckyBastards
+{
+    static class RandomExtension
+    {
+        // https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new
+        public static async Task<IReadOnlyCollection<T>> InRandomOrder<T>(this IEnumerable<T> items)
+        {
+            using (var httpClient = new HttpClient { BaseAddress = new Uri("https://www.random.org") })
+            {
+                var list = items.ToList();
+                var max = list.Count - 1;
+
+                var randomNumbers = new ConcurrentQueue<int>();
+
+                async Task<int> NextRandomIndex()
+                {
+                    while (true)
+                    {
+                        if (randomNumbers.TryDequeue(out var result)) return result;
+
+                        var route = $"integers/?num=100&min=0&max={max}&col=1&base=10&format=plain&rnd=new";
+                        var lines = await httpClient.GetStringAsync(route);
+
+                        foreach (var number in lines.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse))
+                        {
+                            randomNumbers.Enqueue(number);
+                        }
+                    }
+                }
+
+                var iterations = list.Count * 5;
+
+                for (var counter = 0; counter < iterations; counter++)
+                {
+                    var (index1, index2) = (await NextRandomIndex(), await NextRandomIndex());
+
+                    (list[index1], list[index2]) = (list[index2], list[index1]);
+                }
+
+                return list;
+            }
+        }
+    }
+}
