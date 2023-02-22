c36c132cffd7b3730452e27876eb421a0aee5a62
Mogens Heller Grabe <mookid8000@gmail.com>
2023-02-22 17:16:44 +0100

    modernize

--- LuckyBastards/Program.cs
+++ LuckyBastards/Program.cs
@@ -7,89 +7,88 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Tababular;
 
-namespace LuckyBastards
+namespace LuckyBastards;
+
+class Program
 {
-    class Program
+    const string FilePath = @"C:\temp\Azure_Functions_Hello_World__Real_life_usecases.csv";
+
+    static readonly TableFormatter Formatter = new(new Hints { CollapseVerticallyWhenSingleLine = true });
+
+    static async Task Main()
     {
-        const string FilePath = @"C:\temp\Azure_Functions_Hello_World__Real_life_usecases.csv";
+        var lines = await File.ReadAllLinesAsync(FilePath);
 
-        static readonly TableFormatter Formatter = new TableFormatter(new Hints { CollapseVerticallyWhenSingleLine = true });
+        var columns = lines.First().Split('\t');
 
-        static async Task Main()
+        var rows = lines.Skip(1)
+            .Select(line => line.Split('\t'))
+            .Where(cells => cells.Length == columns.Length)
+            .ToList();
+
+        PrintRows(rows, columns);
+
+        var rowsInRandomOrder = await rows.InRandomOrderAsync();
+
+        using (var enumerator = rowsInRandomOrder.GetEnumerator())
         {
-            var lines = File.ReadAllLines(FilePath);
-
-            var columns = lines.First().Split('\t');
-
-            var rows = lines.Skip(1)
-                .Select(line => line.Split('\t'))
-                .Where(cells => cells.Length == columns.Length)
-                .ToList();
-
-            PrintRows(rows, columns);
-
-            var rowsInRandomOrder = await rows.InRandomOrderAsync();
-
-            using (var enumerator = rowsInRandomOrder.GetEnumerator())
+            while (enumerator.MoveNext())
             {
-                while (enumerator.MoveNext())
-                {
-                    Console.WriteLine("Tryk (N) for NÆSTE VINDER, (Q) for at slutte.");
+                Console.WriteLine("Tryk (N) for NÆSTE VINDER, (Q) for at slutte.");
 
-                    if (Console.ReadKey(true).KeyChar == 'q') break;
+                if (Console.ReadKey(true).KeyChar == 'q') break;
 
-                    Console.WriteLine("VINDEREN ER:");
-                    
-                    PrintRows(new[] { enumerator.Current }, columns);
-                    
-                    Console.WriteLine();
+                Console.WriteLine("VINDEREN ER:");
 
-                    var meetupProfileUrl = enumerator.Current.Last();
+                PrintRows(new[] { enumerator.Current }, columns);
 
-                    OpenUrl(meetupProfileUrl);
-                }
+                Console.WriteLine();
+
+                var meetupProfileUrl = enumerator.Current?.Last();
+
+                OpenUrl(meetupProfileUrl);
             }
-
-            Console.WriteLine("Slut");
-            Console.ReadLine();
         }
 
-        static void PrintRows(IEnumerable<string[]> rows, string[] columns)
-        {
-            var dictionaries = rows
-                .Select(cells => cells.Zip(columns, (cell, header) => (cell: cell, header: header)))
-                .Select(tuples => tuples.ToDictionary(a => a.header, a => a.cell))
-                .ToList();
+        Console.WriteLine("Slut");
+        Console.ReadLine();
+    }
 
-            Console.WriteLine(Formatter.FormatDictionaries(dictionaries));
+    static void PrintRows(IEnumerable<string[]> rows, string[] columns)
+    {
+        var dictionaries = rows
+            .Select(cells => cells.Zip(columns, (cell, header) => (cell, header)))
+            .Select(tuples => tuples.ToDictionary(a => a.header, a => a.cell))
+            .ToList();
+
+        Console.WriteLine(Formatter.FormatDictionaries(dictionaries));
+    }
+
+    static void OpenUrl(string url)
+    {
+        try
+        {
+            Process.Start(url);
         }
-
-        static void OpenUrl(string url)
+        catch (Exception exception)
         {
-            try
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                Process.Start(url);
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
             }
-            catch(Exception exception)
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    url = url.Replace("&", "^&");
-                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    Process.Start("xdg-open", url);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    Process.Start("open", url);
-                }
-                else
-                {
-                    Console.WriteLine($"Sorry! Could not open '{url}' because of this: {exception}");
-                }
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                Console.WriteLine($"Sorry! Could not open '{url}' because of this: {exception}");
             }
         }
     }
--- LuckyBastards/TrulyRandomExtension.cs
+++ LuckyBastards/TrulyRandomExtension.cs
@@ -6,49 +6,48 @@ using System.Threading.Tasks;
 // ReSharper disable ArgumentsStyleLiteral
 // ReSharper disable ArgumentsStyleOther
 
-namespace LuckyBastards
+namespace LuckyBastards;
+
+static class TrulyRandomExtension
 {
-    static class TrulyRandomExtension
+    public static async Task<IReadOnlyCollection<T>> InRandomOrderAsync<T>(this IEnumerable<T> items)
     {
-        public static async Task<IReadOnlyCollection<T>> InRandomOrderAsync<T>(this IEnumerable<T> items)
-        {
-            var list = items.ToList();
-            var iterations = list.Count * 5;
+        var list = items.ToList();
+        var iterations = list.Count * 5;
             
-            var numberGenerator = TrulyRandomNumbers(min: 0, max: list.Count - 1).GetAsyncEnumerator();
+        var numberGenerator = TrulyRandomNumbers(min: 0, max: list.Count - 1).GetAsyncEnumerator();
 
-            async Task<int> NextRandomIndex() => await numberGenerator.MoveNextAsync()
-                ? numberGenerator.Current
-                : throw new ApplicationException("Did not expect to reach the end of this sequence");
+        async Task<int> NextRandomIndex() => await numberGenerator.MoveNextAsync()
+            ? numberGenerator.Current
+            : throw new ApplicationException("Did not expect to reach the end of this sequence");
 
-            for (var counter = 0; counter < iterations; counter++)
-            {
-                var index1 = await NextRandomIndex();
-                var index2 = await NextRandomIndex();
+        for (var counter = 0; counter < iterations; counter++)
+        {
+            var index1 = await NextRandomIndex();
+            var index2 = await NextRandomIndex();
 
-                (list[index1], list[index2]) = (list[index2], list[index1]);
-            }
-
-            return list;
+            (list[index1], list[index2]) = (list[index2], list[index1]);
         }
 
-        static async IAsyncEnumerable<int> TrulyRandomNumbers(int min, int max)
+        return list;
+    }
+
+    static async IAsyncEnumerable<int> TrulyRandomNumbers(int min, int max)
+    {
+        using var client = new HttpClient { BaseAddress = new Uri("https://www.random.org") };
+
+        while (true)
         {
-            using var client = new HttpClient { BaseAddress = new Uri("https://www.random.org") };
+            var route = $"integers/?num=100&min={min}&max={max}&col=1&base=10&format=plain&rnd=new";
+            var lines = await client.GetStringAsync(route);
 
-            while (true)
+            var numbers = lines
+                .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            foreach (var number in numbers)
             {
-                var route = $"integers/?num=100&min={min}&max={max}&col=1&base=10&format=plain&rnd=new";
-                var lines = await client.GetStringAsync(route);
-
-                var numbers = lines
-                    .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse);
-
-                foreach (var number in numbers)
-                {
-                    yield return number;
-                }
+                yield return number;
             }
         }
     }
